d8cfb6f59c89904beeafcb85c1d2a107e853ff83
pizdoc <denis.nyukhalov@gmail.com>
2018-09-20 01:09:37 +0600

    readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='ftredis',
     description='minimal wrapper for ebani module',
-    version='0.1',
+    version='0.1.1',
     packages=['ftredis'],
     install_requires=['redis']
 )
