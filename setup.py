af4ff18d9e0244d0009814aab3c739c1853db328
pizdoc <denis.nyukhalov@gmail.com>
2018-09-20 00:04:56 +0600

    init

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='ftredis',
+    description='minimal wrapper for ebani module',
+    version='0.1',
+    packages=['ftredis'],
+    install_requires=['redis']
+)
